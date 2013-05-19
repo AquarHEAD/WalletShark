9fb7837b8b75421c3f3b396f53f6db14a2b58d04
AquarHEAD L <aquarhead@gmail.com>
2013-05-19 23:32:23 +0800

    change association

--- models/prepaid_card.rb
+++ models/prepaid_card.rb
@@ -12,5 +12,6 @@ class PrepaidCard
   property :expire_at, DateTime
   timestamps :created_at
 
-  belongs_to :user, :required => false, :default => User.first()
+  # belongs_to :user, :required => false, :default => User.first()
+  has 1, :user, :required => false
 end
