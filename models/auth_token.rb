c126f69d020907a3ce2dbfd4340d451825e56465
AquarHEAD L <aquarhead@gmail.com>
2013-05-20 00:13:42 +0800

    fix incorret usage of has 1

--- models/auth_token.rb
+++ models/auth_token.rb
@@ -13,7 +13,6 @@ class AuthToken
   property :used_at, DateTime
   timestamps :created_at
 
-  # belongs_to :user, :required => false, :default => User.first()
-  has 1, :user, :required => false
+  belongs_to :user, :required => false
   belongs_to :service_provider
 end
